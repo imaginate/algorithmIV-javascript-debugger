c43b7cfd11d9b5b660d0a00339595acd8669317c
Adam Smith <adamsmith@youlum.com>
2015-03-26 20:59:32 -0700

    Added public method getID to Tests Module

--- tests/pre-compiled-tests/module-methods.js
+++ tests/pre-compiled-tests/module-methods.js
@@ -1,5 +1,17 @@
   /**
    * ---------------------------------------------
+   * Public Method (getID)
+   * ---------------------------------------------
+   * @desc A shortcut for getElementById.
+   * @param {string} title - The name of the id of the element to select.
+   * @return {HTMLElement} A reference to element with the given id.
+   */
+  function getID(title) {
+    return document.getElementById(title);
+  }
+
+  /**
+   * ---------------------------------------------
    * Public Method (generateNumbers)
    * ---------------------------------------------
    * @desc Generates a random number or array of numbers.
