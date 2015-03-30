c490c5eafbb084dc38ff2a5e77979bb7ca54a917
Adam Smith <adamsmith@youlum.com>
2015-03-29 21:54:33 -0700

    Added more options to method (checkType) in module scope

--- tests/pre-compiled-debug/module-methods.js
+++ tests/pre-compiled-debug/module-methods.js
@@ -8,7 +8,7 @@
    */
   function getSubstituteString(val) {
 
-    if (typeof val === 'object') {
+    if ( checkType(val, 'object|function') ) {
       return '%O';
     }
 
@@ -63,12 +63,13 @@
    * Public Method (checkType)
    * ---------------------------------------------------
    * @param {val} val - The value to be evaluated.
-   * @param {string} type - The type to evaluate the value against.
-   *   The optional types are 'string', 'number', 'boolean', 'object',
-   *   'elem', 'undefined', 'array', 'strings', 'numbers', 'booleans',
-   *   'objects', and 'elems'. Use '|' as the separator for multiple
-   *    types (e.g.'strings|numbers'). Use '=' to indicate the value
-   *   is optional (e.g. 'array=' or 'string|number='). Use '!' to
+   * @param {string} type - The type to evaluate the value against. The optional
+   *   types are 'string', 'number', 'boolean', 'object', 'function', 'elem',
+   *   'undefined', 'array', 'strings', 'numbers', 'booleans', 'objects',
+   *   'functions', 'arrays', 'elems', 'stringMap', 'numberMap', 'booleanMap',
+   *   'objectMap', 'functionMap', 'arrayMap', and 'elemMap'. Use '|' as the
+   *   separator for multiple types (e.g.'strings|numbers'). Use '=' to indicate
+   *   the value is optional (e.g. 'array=' or 'string|number='). Use '!' to
    *   indicate that null is not a possibility (e.g. '!string').
    * @return {boolean} The evaluation result.
    */
@@ -85,30 +86,10 @@
     }
 
     /**
-     * @type {RegExp}
-     * @private
-     */
-    var arrays;
-    /**
-     * @type {RegExp}
-     * @private
-     */
-    var simple;
-    /**
-     * @type {RegExp}
-     * @private
-     */
-    var allTypes;
-    /**
      * @type {strings}
      * @private
      */
     var types;
-
-    arrays = /^array$|^strings$|^numbers$|^booleans$|^objects$|^elems$/;
-    simple = /^string$|^number$|^boolean$|^object$/;
-    allTypes = '^elem$|^undefined$|' + simple.source + '|' + arrays.source;
-    allTypes = new RegExp(allTypes);
 
     type = type.toLowerCase().replace(/[^a-z\|\=\!]/g, '');
 
@@ -124,7 +105,7 @@
       cleanType = type.replace(/\!|\=/g, '');
 
       // Ensure a correct type was given
-      if ( !allTypes.test(cleanType) ) {
+      if ( !regexps.types.all.test(cleanType) ) {
         msg = 'A checkType method\'s type was the wrong value. ';
         msg += 'See the docs for acceptable values. ';
         msg += 'The incorrect value was \'%s\'.';
@@ -150,14 +131,22 @@
         }
 
         // Evaluate array types
-        if ( arrays.test(cleanType) ) {
+        if ( regexps.types.arrays.test(cleanType) ) {
 
           if ( !Array.isArray(val) ) {
             return false;
           }
 
+          // Evaluate a basic array
           if (cleanType === 'array') {
             return true;
+          }
+
+          // Evaluate an array of arrays
+          if (cleanType === 'arrays') {
+            return val.every(function(subVal) {
+              return ( Array.isArray(subVal) );
+            });
           }
 
           // Evaluate an array of elements
@@ -180,8 +169,36 @@
         }
 
         // Evaluate string, number, boolean, and object types
-        if ( simple.test(cleanType) ) {
+        if ( regexps.types.basic.test(cleanType) ) {
           return (typeof val === cleanType);
+        }
+
+        // Evaluate hash map types
+        if ( regexps.types.maps.test(cleanType) ) {
+
+          if (typeof val !== 'object') {
+            return false;
+          }
+
+          // Evaluate a hash map of arrays
+          if (cleanType === 'arrayMap') {
+            return Object.keys(val).every(function(subVal) {
+              return ( Array.isArray(val[ subVal ]) );
+            });
+          }
+
+          // Evaluate a hash map of elements
+          if (cleanType === 'elems') {
+            return Object.keys(val).every(function(subVal) {
+              return (val[ subVal ] instanceof HTMLElement);
+            });
+          }
+
+          // Evaluate each value of the hash map
+          cleanType = cleanType.replace(/Map$/, '');
+          return Object.keys(val).every(function(subVal) {
+            return (typeof val[ subVal ] === cleanType);
+          });
         }
       }
 
