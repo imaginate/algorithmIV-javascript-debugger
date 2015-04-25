4ae2a6c916fcbd1414d0462db2f3849262883c69
Adam Smith <adamsmith@youlum.com>
2015-04-25 14:21:40 -0700

    Created the ErrorMessages class

---
+++ tests/pre-compiled-debug/classes/error-messages.js
@@ -0,0 +1,11 @@
+  /**
+   * ----------------------------------------------- 
+   * Public Class (ErrorMessages)
+   * -----------------------------------------------
+   * @desc Error messages used throughout this module.
+   * @type {!Object<string, string>}
+   * @struct
+   */
+  var ErrorMessages = {};
+
+  Object.freeze(ErrorMessages);
