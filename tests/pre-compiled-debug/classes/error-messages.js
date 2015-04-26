a0605a804e4d2cc4e780faa6fa862786e8fa1abd
Adam Smith <adamsmith@youlum.com>
2015-04-25 17:11:28 -0700

    Added ErrorMessages.missingMethodName

--- tests/pre-compiled-debug/classes/error-messages.js
+++ tests/pre-compiled-debug/classes/error-messages.js
@@ -1,11 +1,35 @@
   /**
-   * ----------------------------------------------- 
+   * ---------------------------------------------------
    * Public Class (ErrorMessages)
-   * -----------------------------------------------
+   * ---------------------------------------------------
    * @desc Error messages used throughout this module.
-   * @type {!Object<string, string>}
+   * @type {!Object<string, (string|function)>}
    * @struct
    */
   var ErrorMessages = {};
 
-  Object.freeze(ErrorMessages);
+  /**
+   * ---------------------------------------------------
+   * Public Property (ErrorMessages.missingMethodName)
+   * ---------------------------------------------------
+   * @desc Creates an error message for a missing method name
+   *   parameter in a Debug logging method.
+   * @param {string} method - The name of the method that failed.
+   * @param {string} methodName - The user's method name parameter.
+   * @return {string} The error message.
+   */
+  ErrorMessages.missingMethodName = function(method, methodName) {
+
+    /** @type {string} */
+    var message;
+
+    message = 'An aIV.console ' + method + ' call was missing a valid method ';
+    message += 'name parameter (the first parameter). It should be a string ';
+    message += 'of the method\'s name that ' + method + ' is recording. The ';
+    message += 'invalid method name parameter\'s data type follows: ';
+    message += (methodName === null) ? 'null' : typeof methodName;
+
+    return message;
+  };
+
+  freezeObj(ErrorMessages, true);
