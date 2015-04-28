accd5a70c39ff76941961ec96310a5cb416bbdcc
Adam Smith <adamsmith@youlum.com>
2015-04-27 18:44:10 -0700

    Added hasOwnProp to tests

--- tests/pre-compiled-tests/module-methods.js
+++ tests/pre-compiled-tests/module-methods.js
@@ -9,3 +9,32 @@
   function getID(title) {
     return document.getElementById(title);
   }
+
+  /**
+   * ---------------------------------------------------
+   * Public Method (hasOwnProp)
+   * ---------------------------------------------------
+   * @desc A shortcut for the Object.prototype.hasOwnProperty method.
+   * @param {!object|function} obj - The object to check.
+   * @param {string} prop - The property to check.
+   * @return {boolean} The result of the check.
+   */
+  function hasOwnProp(obj, prop) {
+
+    /** @type {string} */
+    var errorMessage;
+
+    if (!obj || typeof obj !== 'object' || typeof obj !== 'function') {
+      errorMessage = 'A hasOwnProp call received an invalid obj parameter.';
+      throw new TypeError(errorMessage);
+      return;
+    }
+
+    if (!prop || typeof prop !== 'string') {
+      errorMessage = 'A hasOwnProp call received an invalid prop parameter.';
+      throw new TypeError(errorMessage);
+      return;
+    }
+
+    return obj.hasOwnProperty(prop);
+  }
