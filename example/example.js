d79a4252ba66db2d2d886f892a18dd97a3233a70
Adam Smith <adamsmith@youlum.com>
2015-05-01 05:32:39 -0700

    Updated the Example

--- example/example.js
+++ example/example.js
@@ -24,9 +24,6 @@ var Example = function(name, check) {
    */
   this.debug = aIV.debug('Example');
 
-  // Start a collapsed console group
-  this.debug.group('init', 'open', 'name= $$, check= $$', name, check);
-
   // Log the start of a new instance of Example
   this.debug.start('init', name, check);
 
@@ -42,8 +39,11 @@ var Example = function(name, check) {
    */
   this.getCheck = function() {
 
-    // Log the start of this method
+    // Start this method
     this.debug.start('getCheck');
+
+    // End this method
+    this.debug.end('getCheck', check);
 
     return check;
   };
@@ -57,17 +57,19 @@ var Example = function(name, check) {
    */
   this.getName = function() {
 
-    // Log the start of this method
+    // Start this method
     this.debug.start('getName');
+
+    // End this method
+    this.debug.end('getName', name);
 
     return name;
   };
 
-  // Close the open console group
-  this.debug.group('init', 'end');
+  // End this method
+  this.debug.end('init');
 };
 
-// Ensure constructor is set to this class.
 Example.prototype.constructor = Example;
 
 /**
@@ -79,13 +81,15 @@ Example.prototype.constructor = Example;
  */
 Example.prototype.report = function() {
 
-  // Log the start of this method
+  // Start this method
   this.debug.start('report');
 
   /** @type {string} */
   var name;
   /** @type {boolean} */
   var check;
+  /** @type {string} */
+  var result;
 
   name = this.getName();
   check = this.getCheck();
@@ -93,7 +97,12 @@ Example.prototype.report = function() {
   // Catch failed reports 
   this.debug.fail('report', check, '$$\'s report failed.', name);
 
-  return name + ' => ' + ( (check) ? 'Pass' : 'Fail' );
+  result = name + ' => ' + ( (check) ? 'Pass' : 'Fail' );
+
+  // End this method
+  this.debug.end('report', result);
+
+  return result;
 };
 
 ////////////////////////////////////////////////////////////////////////////////
@@ -107,55 +116,68 @@ var name;
 /** @type {boolean} */
 var check;
 
+// You can set the default settings for your debug instances
+aIV.debug.set({
+  addBreakpoints: 'none',
+  turnOnGroups  : true,
+  turnOnTimers  : true
+});
+
 // When the Example constructor is called its debug methods create a new
 // console group (debug.group), log its start (debug.start), check its
 // arguments (debug.args), and end the group (debug.group)
 example.test1 = new Example('Test 1', true);
 example.test2 = new Example('Test 2', false);
 
+// Start the testLogic examples
+example.test1.debug.start('testLogic');
+
 // debug.args will catch the incorrect argument type and log an error
 name = 1;
-example.test1.debug.args('logic', name, 'string');
+example.test1.debug.args('testLogic', name, 'string');
 
 // debug.args can check for more complex types (first check passes and the
 // second check fails)
 name = [ 'name', 'name' ];
-example.test1.debug.args('logic', name, 'strings');
+example.test1.debug.args('testLogic', name, 'strings');
 name = [ 1, 1 ];
-example.test1.debug.args('logic', name, 'strings');
+example.test1.debug.args('testLogic', name, 'strings');
 
 // debug.state shares the state of variables at any moment
 name = 'Test 1';
 check = true;
-example.test1.debug.state('logic', 'name= $$, check= $$', name, check);
+example.test1.debug.state('testLogic', 'name= $$, check= $$', name, check);
 
 // You can turn off methods with debug.turnOff or in the constructor
-example.test1.debug.turnOff('misc');
-example.test1.debug.misc('logic', 'This message is NOT logged.');
+example.test1.debug.turnOffMethod('misc');
+example.test1.debug.misc('testLogic', 'This message is NOT logged.');
 
 // You can turn on methods with debug.turnOn (all methods are turned on by
 // default)
-example.test1.debug.turnOn('misc');
-example.test1.debug.misc('logic', 'This message is logged.');
+example.test1.debug.turnOnMethod('misc');
+example.test1.debug.misc('testLogic', 'This message is logged.');
 
 // You can request that any method insert a debugger instance after their logs
 // with debug.turnOnDebugger or in the constructor
-example.test1.debug.turnOnDebugger('misc');
-example.test1.debug.misc('logic', 'A debugger instance should appear.');
+example.test1.debug.addBreakpoint('misc');
+example.test1.debug.misc('testLogic', 'A debugger instance should appear.');
 
 // You can remove debugger instances with debug.turnOffDebugger (all debugger
 // instances are turned off by default)
-example.test1.debug.turnOffDebugger('misc');
-example.test1.debug.misc('logic', 'A debugger instance should NOT appear.');
+example.test1.debug.removeBreakpoint('misc');
+example.test1.debug.misc('testLogic', 'A debugger instance should NOT appear.');
 
 // You can add varaiables easily to your log messages with $$
-example.test1.debug.misc('logic', 'This log recorded $$\'s name.', name);
+example.test1.debug.misc('testLogic', 'This log recorded $$\'s name.', name);
 
 // The following is an example of catching a bad report from an instance of the
 // example class that could create a bug in a full app (debug.fail creates an
 // error log for the 'Test 2' report)
 example.test1.report();
 example.test2.report();
+
+// End the testLogic examples
+example.test1.debug.end('testLogic');
 
 ////////////////////////////////////////////////////////////////////////////////
 
