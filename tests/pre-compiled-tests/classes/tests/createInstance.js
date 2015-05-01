2e85cacaf41343827622eb80f3e563ba64f3075e
Adam Smith <adamsmith@youlum.com>
2015-04-30 21:53:30 -0700

    Moved the classTitle tests to within createInstance

--- tests/pre-compiled-tests/classes/tests/createInstance.js
+++ tests/pre-compiled-tests/classes/tests/createInstance.js
@@ -2,17 +2,17 @@
    * -------------------------------------------------
    * Public Method (Tests.createInstance)
    * -------------------------------------------------
-   * @desc Checks aIV.utils.createInstance method.
+   * @desc Tests the aIV.console.create method's functionality.
    * @type {function}
    */
-  Tests.createInstance = (function setupTestsCreateInstance() {
+  Tests.createInstance = (function setupTests_createInstance() {
 
     ////////////////////////////////////////////////////////////////////////////
     // Define & Setup The Private createInstance Variables
     ////////////////////////////////////////////////////////////////////////////
 
     /** @type {!TestResults} */
-    var results = new TestResults('Tests.createInstance');
+    var results = new TestResults('aIV.console.create');
 
     ////////////////////////////////////////////////////////////////////////////
     // Define & Setup The Public createInstance Method
@@ -22,13 +22,23 @@
      * -------------------------------------------------
      * Public Method (createInstance)
      * -------------------------------------------------
-     * @desc Checks aIV.utils.createInstance method.
+     * @desc Tests aIV.console.create.
      * @type {function}
      */
     var createInstance = function() {
 
-      testInitInstance();
-      testRecallInstance();
+      // Ensure the Debug object instance creation occurs correctly
+      testOneInstanceCreate();
+      testTwoInstanceCreateSame();
+      testTwoInstanceCreateDifferent();
+
+      // The remaining tests ensure that aIV.console.create is reading and
+      // using its params correctly
+
+      // Test the classTitle param
+      testClassTitleAsProp();
+      testClassTitleAsString();
+      testClassTitleAsBlank();
 
       // Save the results
       app.results.push(results);
@@ -40,11 +50,11 @@
 
     /**
      * ---------------------------------------------------
-     * Private Method (testInitInstance)
+     * Private Method (testOneInstanceCreate)
      * ---------------------------------------------------
      * @type {function}
      */
-    var testInitInstance = function() {
+    var testOneInstanceCreate = function() {
 
       /** @type {boolean} */
       var pass;
@@ -53,23 +63,23 @@
       /** @type {!Debug} */
       var consoleInst;
 
-      consoleInst = aIV.console.create('Tests.checkInstances');
+      consoleInst = aIV.console.create('createInst.testOneInstanceCreate');
 
       pass = Object.prototype.toString.call(consoleInst) === '[object Debug]';
 
       if (!pass) {
-        errorMsg = 'createInstance failed to create a Debug instance';
+        errorMsg = 'aIV.console.create failed to create a Debug instance';
         results.addError(errorMsg);
       }
     };
 
     /**
      * ---------------------------------------------------
-     * Private Method (testRecallInstance)
+     * Private Method (testTwoInstanceCreateSame)
      * ---------------------------------------------------
      * @type {function}
      */
-    var testRecallInstance = function() {
+    var testTwoInstanceCreateSame = function() {
 
       /** @type {boolean} */
       var pass;
@@ -77,28 +87,164 @@
       var fail;
       /** @type {string} */
       var errorMsg;
+      /** @type {string} */
+      var classTitle;
       /** @type {!Debug} */
       var consoleInst1;
       /** @type {!Debug} */
       var consoleInst2;
 
-      consoleInst1 = aIV.console.create('Tests.checkInstances');
+      classTitle = 'createInst.testTwoInstanceCreateSame';
+
+      consoleInst1 = aIV.console.create(classTitle);
       consoleInst2 = aIV.console.create({
-        classTitle    : 'Tests.checkInstances',
+        classTitle    : classTitle,
         turnOffMethods: 'misc'
       });
 
-      pass = consoleInst1.misc('test1', 'Instance Test 1');
-      pass = pass && consoleInst2.misc('test2', 'Instance Test 2');
+      pass = consoleInst1.misc('test', 'Instance Test 1');
+      pass = pass && consoleInst2.misc('test', 'Instance Test 2');
 
       consoleInst1.setMethod('misc', false);
 
-      fail = consoleInst1.misc('test3', 'Instance Test 3');
-      fail = fail || consoleInst2.misc('test4', 'Instance Test 4');
+      fail = consoleInst1.misc('test', 'Instance Test 3');
+      fail = fail || consoleInst2.misc('test', 'Instance Test 4');
 
       if (!pass || fail) {
-        errorMsg = 'createInstance failed to toggle misc correctly ';
-        errorMsg += 'across instances';
+        errorMsg = 'aIV.console.create incorrectly created a new Debug ';
+        errorMsg += 'instance when it should have retrieved an existing one';
+        results.addError(errorMsg);
+      }
+    };
+
+    /**
+     * ---------------------------------------------------
+     * Private Method (testTwoInstanceCreateDifferent)
+     * ---------------------------------------------------
+     * @type {function}
+     */
+    var testTwoInstanceCreateDifferent = function() {
+
+      /** @type {boolean} */
+      var pass;
+      /** @type {boolean} */
+      var fail;
+      /** @type {string} */
+      var errorMsg;
+      /** @type {string} */
+      var classTitle;
+      /** @type {!Debug} */
+      var consoleInst1;
+      /** @type {!Debug} */
+      var consoleInst2;
+
+      classTitle = 'createInst.testTwoInstanceCreateDifferent';
+      consoleInst1 = aIV.console.create(classTitle);
+      consoleInst2 = aIV.console.create(classTitle + '2');
+
+      pass = consoleInst1.misc('test', 'Instance Test 1');
+      consoleInst1.setMethod('misc', false);
+      fail = consoleInst1.misc('test', 'Instance Test 2');
+
+      pass = pass && consoleInst2.misc('test', 'Instance Test 3');
+      consoleInst2.setMethod('misc', false);
+      fail = fail || consoleInst2.misc('test', 'Instance Test 4');
+
+      if (!pass || fail) {
+        errorMsg = 'aIV.console.create failed to create two separate Debug ';
+        errorMsg += 'instances correctly';
+        results.addError(errorMsg);
+      }
+    };
+
+    /**
+     * ---------------------------------------------------
+     * Private Method (testClassTitleAsProp)
+     * ---------------------------------------------------
+     * @type {function}
+     */
+    var testClassTitleAsProp = function() {
+
+      /** @type {boolean} */
+      var pass;
+      /** @type {string} */
+      var errorMsg;
+      /** @type {string} */
+      var classTitle;
+      /** @type {!Debug} */
+      var consoleInst;
+
+      classTitle = 'createInst.testClassTitleAsProp';
+
+      consoleInst = aIV.console.create({
+        classTitle: classTitle
+      });
+
+      classTitle += '.';
+
+      pass = (consoleInst.classTitle === classTitle);
+
+      if (!pass) {
+        errorMsg = 'aIV.console.create({ classTitle: \'name\' }) failed to ';
+        errorMsg += 'correctly setup the class title';
+        results.addError(errorMsg);
+      }
+    };
+
+    /**
+     * ---------------------------------------------------
+     * Private Method (testClassTitleAsString)
+     * ---------------------------------------------------
+     * @type {function}
+     */
+    var testClassTitleAsString = function() {
+
+      /** @type {boolean} */
+      var pass;
+      /** @type {string} */
+      var errorMsg;
+      /** @type {string} */
+      var classTitle;
+      /** @type {!Debug} */
+      var consoleInst;
+
+      classTitle = 'createInst.testClassTitleAsString';
+
+      consoleInst = aIV.console.create(classTitle);
+
+      classTitle += '.';
+
+      pass = (consoleInst.classTitle === classTitle);
+
+      if (!pass) {
+        errorMsg = 'aIV.console.create(\'name\') failed to correctly setup the ';
+        errorMsg += 'class title';
+        results.addError(errorMsg);
+      }
+    };
+
+    /**
+     * ---------------------------------------------------
+     * Private Method (testClassTitleAsBlank)
+     * ---------------------------------------------------
+     * @type {function}
+     */
+    var testClassTitleAsBlank = function() {
+
+      /** @type {boolean} */
+      var pass;
+      /** @type {string} */
+      var errorMsg;
+      /** @type {!Debug} */
+      var consoleInst;
+
+      consoleInst = aIV.console.create();
+
+      pass = (consoleInst.classTitle === 'unknown.');
+
+      if (!pass) {
+        errorMsg = 'aIV.console.create() failed to correctly setup the ';
+        errorMsg += '\'unknown\' class title';
         results.addError(errorMsg);
       }
     };
