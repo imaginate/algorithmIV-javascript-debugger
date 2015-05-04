299661b7da6167de0df40c3615f5c04458c7d8db
Adam Smith <adamsmith@youlum.com>
2015-05-04 16:39:41 -0700

    Added checkType to the Tests Module

--- tests/pre-compiled-tests/module-methods.js
+++ tests/pre-compiled-tests/module-methods.js
@@ -20,3 +20,18 @@
    * @return {boolean} The result of the check.
    */
   var hasOwnProp = aIV.utils.hasOwnProp;
+
+  /**
+   * ---------------------------------------------------
+   * Public Method (checkType)
+   * ---------------------------------------------------
+   * @desc Checks a value's data type against the given optional types.
+   * @param {*} val - The value to be evaluated.
+   * @param {string} type - A string of the data types to evaluate the value
+   *   against. For a complete list of acceptable strings
+   *   [see aIV.utils.checkType]{@link https://github.com/imaginate/algorithmIV-javascript-shortcuts/blob/master/src/pre-compiled-parts/methods/checkType.js}.
+   * @param {boolean=} noTypeValCheck - If true skips the data type string checks.
+   *   The default is false. Use to avoid duplicating checks.
+   * @return {boolean} The evaluation result.
+   */
+  var checkType = aIV.utils.checkType;
