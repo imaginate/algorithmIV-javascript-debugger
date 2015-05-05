0d5906d67af81850ee744043f17f8b36c6f7c498
Adam Smith <adamsmith@youlum.com>
2015-05-04 22:51:27 -0700

    Added MockConsole to Tests Module

---
+++ tests/pre-compiled-tests/classes/mock-console.js
@@ -0,0 +1,175 @@
+  /**
+   * -----------------------------------------------------
+   * Public Class (MockConsole)
+   * -----------------------------------------------------
+   * @desc Mocks the Console class for testing.
+   * @constructor
+   */
+  var MockConsole = function() {
+
+    ////////////////////////////////////////////////////////////////////////////
+    // Define & Setup The Public Properties
+    ////////////////////////////////////////////////////////////////////////////
+
+    /**
+     * ----------------------------------------------- 
+     * Public Property (MockConsole.logs)
+     * -----------------------------------------------
+     * @desc The logs made.
+     * @type {!strings}
+     */
+    this.logs = [];
+
+    ////////////////////////////////////////////////////////////////////////////
+    // Define & Setup The Protected Properties
+    ////////////////////////////////////////////////////////////////////////////
+
+    /**
+     * ----------------------------------------------- 
+     * Protected Property (MockConsole.originals)
+     * -----------------------------------------------
+     * @desc The original console methods (that will be temporarily overridden).
+     * @type {!Object<string, function>}
+     */
+    var originals = {
+      log     : console.log,
+      error   : console.error,
+      group   : console.group,
+      groupEnd: console.groupEnd
+    };
+
+    /**
+     * ----------------------------------------------- 
+     * Protected Property (MockConsole.that)
+     * -----------------------------------------------
+     * @desc The MockConsole Instance.
+     * @type {!Object<string, !strings>}
+     */
+    var that = this;
+
+    ////////////////////////////////////////////////////////////////////////////
+    // Define & Setup The Public Methods
+    ////////////////////////////////////////////////////////////////////////////
+
+    /**
+     * ----------------------------------------------- 
+     * Public Method (MockConsole.reset)
+     * -----------------------------------------------
+     * @desc Resets the global Console object to its orginal settings.
+     * @type {function}
+     */
+    this.reset = function() {
+      console.log = originals.log;
+      console.error = originals.error;
+      console.group = originals.group;
+      console.groupEnd = originals.groupEnd;
+    };
+
+    ////////////////////////////////////////////////////////////////////////////
+    // Define & Setup The Mock Methods
+    ////////////////////////////////////////////////////////////////////////////
+
+    /**
+     * -----------------------------------------------
+     * Mock Method (MockConsole.log)
+     * -----------------------------------------------
+     * @type {function}
+     */
+    console.log = function(message) {
+
+      /** @type {string} */
+      var log;
+      /** @type {!strings} */
+      var args;
+
+      args = ( (arguments.length > 1) ?
+        Array.prototype.slice.call(arguments, 1) : []
+      );
+
+      log = 'LOG: ' + message;
+      if (args.length) {
+        log += ' ' + args.join(' ');
+      }
+
+      that.logs.push(log);
+    };
+
+    /**
+     * -----------------------------------------------
+     * Mock Method (MockConsole.error)
+     * -----------------------------------------------
+     * @type {function}
+     */
+    console.error = function(message) {
+
+      /** @type {string} */
+      var log;
+      /** @type {!strings} */
+      var args;
+
+      args = ( (arguments.length > 1) ?
+        Array.prototype.slice.call(arguments, 1) : []
+      );
+
+      log = 'ERROR: ' + message;
+      if (args.length) {
+        log += ' ' + args.join(' ');
+      }
+
+      that.logs.push(log);
+    };
+
+    /**
+     * -----------------------------------------------
+     * Mock Method (MockConsole.log)
+     * -----------------------------------------------
+     * @type {function}
+     */
+    console.group = function(message) {
+
+      /** @type {string} */
+      var log;
+      /** @type {!strings} */
+      var args;
+
+      args = ( (arguments.length > 1) ?
+        Array.prototype.slice.call(arguments, 1) : []
+      );
+
+      log = 'OPEN GROUP: ' + message;
+      if (args.length) {
+        log += ' ' + args.join(' ');
+      }
+
+      that.logs.push(log);
+    };
+
+    /**
+     * -----------------------------------------------
+     * Mock Method (MockConsole.log)
+     * -----------------------------------------------
+     * @type {function}
+     */
+    console.groupEnd = function() {
+
+      /** @type {string} */
+      var log;
+
+      log = 'CLOSE GROUP';
+
+      that.logs.push(log);
+    };
+
+    ////////////////////////////////////////////////////////////////////////////
+    // End Of The Class Setup
+    ////////////////////////////////////////////////////////////////////////////
+
+    // Deep freeze
+    aIV.utils.freezeObj(this, true);
+  };
+
+////////////////////////////////////////////////////////////////////////////////
+// The Prototype Methods
+////////////////////////////////////////////////////////////////////////////////
+
+  MockConsole.prototype.constructor = MockConsole;
