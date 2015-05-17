3f2c837ddbe5ed569380da3816409a6cf2007c75
Adam Smith <adamsmith@youlum.com>
2015-05-17 16:37:05 -0700

    Changed Debug.DEFAULTS to Debug_DEFAULTS

--- tests/pre-compiled-debug/classes/debug/helper-properties.js
+++ tests/pre-compiled-debug/classes/debug/helper-properties.js
@@ -1,48 +1,6 @@
   /**
    * -----------------------------------------------
-   * Public Property (Debug.DEFAULTS)
-   * -----------------------------------------------
-   * @desc The original default settings for a new Debug instance.
-   * @type {!{
-   *   classTitle    : string,
-   *   turnOffMethods: string,
-   *   addBreakpoints: string,
-   *   turnOnGroups  : boolean,
-   *   openGroups    : boolean,
-   *   turnOnProfiles: boolean,
-   *   turnOnTimers  : boolean
-   * }}
-   */
-  Debug.DEFAULTS = {
-    classTitle    : 'unknown',
-    turnOffMethods: 'none',
-    addBreakpoints: 'args fail',
-    turnOnGroups  : true,
-    openGroups    : false,
-    turnOnProfiles: false,
-    turnOnTimers  : false
-  };
-
-  /**
-   * -----------------------------------------------
-   * Public Property (Debug.DEFAULT_TYPES)
-   * -----------------------------------------------
-   * @desc The data types for each settings property.
-   * @type {!Object<string, string>}
-   */
-  Debug.DEFAULT_TYPES = {
-    classTitle    : 'string',
-    turnOffMethods: 'string|!strings',
-    addBreakpoints: 'string|!strings',
-    turnOnGroups  : 'boolean',
-    openGroups    : 'boolean',
-    turnOnProfiles: 'boolean',
-    turnOnTimers  : 'boolean'
-  };
-
-  /**
-   * -----------------------------------------------
-   * Public Property (Debug.defaultSettings)
+   * Public Property (Debug.defaults)
    * -----------------------------------------------
    * @desc Sets default settings for all instances of the debugger. Note that
    *   if local settings are provided upon a new instance call they will be used
@@ -56,14 +14,14 @@
    *   turnOnTimers  : boolean
    * }}
    */
-  Debug.defaultSettings = {
-    classTitle    : Debug.DEFAULTS.classTitle,
-    turnOffMethods: Debug.DEFAULTS.turnOffMethods,
-    addBreakpoints: Debug.DEFAULTS.addBreakpoints,
-    turnOnGroups  : Debug.DEFAULTS.turnOnGroups,
-    openGroups    : Debug.DEFAULTS.openGroups,
-    turnOnProfiles: Debug.DEFAULTS.turnOnProfiles,
-    turnOnTimers  : Debug.DEFAULTS.turnOnTimers
+  Debug.defaults = {
+    classTitle    : Debug_DEFAULTS.classTitle,
+    turnOffMethods: Debug_DEFAULTS.turnOffMethods,
+    addBreakpoints: Debug_DEFAULTS.addBreakpoints,
+    turnOnGroups  : Debug_DEFAULTS.turnOnGroups,
+    openGroups    : Debug_DEFAULTS.openGroups,
+    turnOnProfiles: Debug_DEFAULTS.turnOnProfiles,
+    turnOnTimers  : Debug_DEFAULTS.turnOnTimers
   };
 
   /**
--- tests/pre-compiled-debug/module-vars.js
+++ tests/pre-compiled-debug/module-vars.js
@@ -15,3 +15,45 @@
    * @type {boolean}
    */
   var errorBreakpoints = true;
+
+  /**
+   * -----------------------------------------------
+   * Public Variable (Debug_DEFAULTS)
+   * -----------------------------------------------
+   * @desc The original default settings for a new Debug instance.
+   * @type {!{
+   *   classTitle    : string,
+   *   turnOffMethods: string,
+   *   addBreakpoints: string,
+   *   turnOnGroups  : boolean,
+   *   openGroups    : boolean,
+   *   turnOnProfiles: boolean,
+   *   turnOnTimers  : boolean
+   * }}
+   */
+  var Debug_DEFAULTS = {
+    classTitle    : 'unknown',
+    turnOffMethods: 'none',
+    addBreakpoints: 'args fail',
+    turnOnGroups  : true,
+    openGroups    : false,
+    turnOnProfiles: false,
+    turnOnTimers  : false
+  };
+
+  /**
+   * -----------------------------------------------
+   * Public Variable (Debug_DEFAULT_TYPES)
+   * -----------------------------------------------
+   * @desc The data types for each settings property.
+   * @type {!Object<string, string>}
+   */
+  var Debug_DEFAULT_TYPES = {
+    classTitle    : 'string',
+    turnOffMethods: 'string|!strings',
+    addBreakpoints: 'string|!strings',
+    turnOnGroups  : 'boolean',
+    openGroups    : 'boolean',
+    turnOnProfiles: 'boolean',
+    turnOnTimers  : 'boolean'
+  };
