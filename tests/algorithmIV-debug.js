5b614311b911d67da7b19c3b3fb47c33708d1ff1
Adam Smith <adamsmith@youlum.com>
2015-05-17 18:44:26 -0700

    Moved the Module API's declaration after Module Vars & Methods

--- tests/algorithmIV-debug.js
+++ tests/algorithmIV-debug.js
@@ -238,6 +238,282 @@ new TypeError(a);return!0}}();e.reset=function(){var b,a,c;b=(b=arguments.length
   "use strict";
 
 /* -----------------------------------------------------------------------------
+ * The Public Module Variables (module-vars.js)
+ * -------------------------------------------------------------------------- */
+
+  /**
+   * -----------------------------------------------------
+   * Public Variable (debugInstances)
+   * -----------------------------------------------------
+   * @desc Saves a reference to all of the created Debug instances.
+   * @type {!Object<string, !Debug>}
+   */
+  var debugInstances = {};
+
+  /**
+   * ----------------------------------------------- 
+   * Public Variable (errorBreakpoints)
+   * -----------------------------------------------
+   * @desc Controls whether debugger breakpoints are included with error logs.
+   * @type {boolean}
+   */
+  var errorBreakpoints = true;
+
+  /**
+   * -----------------------------------------------
+   * Public Variable (Debug_DEFAULTS)
+   * -----------------------------------------------
+   * @desc The original default settings for a new Debug instance.
+   * @type {!{
+   *   classTitle    : string,
+   *   turnOffMethods: string,
+   *   addBreakpoints: string,
+   *   turnOnGroups  : boolean,
+   *   openGroups    : boolean,
+   *   turnOnProfiles: boolean,
+   *   turnOnTimers  : boolean
+   * }}
+   */
+  var Debug_DEFAULTS = {
+    classTitle    : 'unknown',
+    turnOffMethods: 'none',
+    addBreakpoints: 'args fail',
+    turnOnGroups  : true,
+    openGroups    : false,
+    turnOnProfiles: false,
+    turnOnTimers  : false
+  };
+
+  /**
+   * -----------------------------------------------
+   * Public Variable (Debug_DEFAULT_TYPES)
+   * -----------------------------------------------
+   * @desc The data types for each settings property.
+   * @type {!Object<string, string>}
+   */
+  var Debug_DEFAULT_TYPES = {
+    classTitle    : 'string',
+    turnOffMethods: 'string|!strings',
+    addBreakpoints: 'string|!strings',
+    turnOnGroups  : 'boolean',
+    openGroups    : 'boolean',
+    turnOnProfiles: 'boolean',
+    turnOnTimers  : 'boolean'
+  };
+
+/* -----------------------------------------------------------------------------
+ * The Public Module Methods (module-methods.js)
+ * -------------------------------------------------------------------------- */
+
+  /**
+   * -----------------------------------------------------
+   * Public Method (makeNewDebugInst)
+   * -----------------------------------------------------
+   * @desc Creates a new Debug object instance. For more details about the
+   *   parameters [see debugModuleAPI.init]{@link debugModuleAPI#init}.
+   * @param {string} classTitle
+   * @param {Object} settings
+   * @param {(string|!strings)=} settings.turnOffMethods
+   * @param {(string|!strings)=} settings.addBreakpoints
+   * @param {boolean=} settings.turnOnGroups
+   * @param {boolean=} settings.openGroups
+   * @param {boolean=} settings.turnOnProfiles
+   * @param {boolean=} settings.turnOnTimers
+   */
+  function makeNewDebugInst(classTitle, settings) {
+
+    /** @type {!Object} */
+    var defaultTypes;
+    /** @type {!Object} */
+    var newSettings;
+    /** @type {!Object} */
+    var defaults;
+    /** @type {string} */
+    var propName;
+    /** @type {*} */
+    var propVal;
+
+    newSettings = {};
+    defaults = Debug.defaults;
+
+    // Set the new instance's settings to the defaults
+    for (propName in defaults) {
+      if ( hasOwnProp(defaults, propName) ) {
+        newSettings[ propName ] = defaults[ propName ];
+      }
+    }
+
+    // Update the new instance's settings with any local settings
+    if (settings) {
+      defaultTypes = Debug_DEFAULT_TYPES;
+      for (propName in settings) {
+        if (hasOwnProp(settings, propName) && hasOwnProp(defaults, propName)) {
+          propVal = settings[ propName ];
+          if ( checkType(propVal, defaultTypes[ propName ]) ) {
+            if ( checkType(propVal, '!strings') ) {
+              propVal = propVal.join(' ');
+            }
+            newSettings[ propName ] = propVal;
+          }
+        }
+      }
+    }
+
+    // Update the new instance's class title
+    newSettings.classTitle = classTitle;
+
+    // Setup and save the new Debug instance
+    debugInstances[ classTitle ] = new Debug(newSettings);
+  };
+
+  /**
+   * -----------------------------------------------------
+   * Public Method (insertErrorBreakpoint)
+   * -----------------------------------------------------
+   * @desc Handles whether a debugger breakpoint is inserted for an error.
+   * @return {boolean} Whether a breakpoint was inserted.
+   */
+  function insertErrorBreakpoint() {
+
+    if (errorBreakpoints) {
+      debugger;
+    }
+
+    return errorBreakpoints;
+  };
+
+  /**
+   * ---------------------------------------------------
+   * Public Method (freezeObj)
+   * ---------------------------------------------------
+   * @desc A shortcut for the Object.freeze method with a deep freeze option.
+   * @param {!(Object|function)} obj - The object to freeze.
+   * @param {boolean=} deep - Deep freeze the object. The default is false.
+   * @return {!(Object|function)} The frozen object.
+   */
+  var freezeObj = aIV.utils.freezeObj;
+
+  /**
+   * ---------------------------------------------------
+   * Public Method (hasOwnProp)
+   * ---------------------------------------------------
+   * @desc A shortcut for the Object.prototype.hasOwnProperty method.
+   * @param {!(Object|function)} obj - The object to check.
+   * @param {string} prop - The property to check.
+   * @return {boolean} The result of the check.
+   */
+  var hasOwnProp = aIV.utils.hasOwnProp;
+
+  /**
+   * ---------------------------------------------------
+   * Public Method (checkType)
+   * ---------------------------------------------------
+   * @desc Checks a value's data type against the given optional types.
+   * @param {*} val - The value to be evaluated.
+   * @param {string} type - A string of the data types to evaluate the value
+   *   against. For a complete list of acceptable strings
+   *   [see aIV.utils.checkType]{@link https://github.com/imaginate/algorithmIV-javascript-shortcuts/blob/master/src/pre-compiled-parts/js-methods/checkType.js}.
+   * @param {boolean=} noTypeValCheck - If true skips the data type string checks.
+   *   The default is false. Use to avoid duplicating checks.
+   * @return {boolean} The evaluation result.
+   */
+  var checkType = aIV.utils.checkType;
+
+  /**
+   * ---------------------------------------------------
+   * Public Method (isValidTypeString)
+   * ---------------------------------------------------
+   * @desc Evaluates whether a string is a valid data type string.
+   * @param {string} type - The string to evaluate.
+   * @return {boolean} The evaluation result.
+   */
+  var isValidTypeString = aIV.utils.isValidTypeString;
+
+  /**
+   * ---------------------------------------------------
+   * Public Method (getTypeOf)
+   * ---------------------------------------------------
+   * @desc A shortcut for the native typeof operator that additionally
+   *   distinguishes null, array, document, and element types from an
+   *   object type.
+   * @param {*} val - The value to get the typeof.
+   * @return {string} The value's type.
+   */
+  var getTypeOf = aIV.utils.getTypeOf;
+
+  /**
+   * -----------------------------------------------------
+   * Public Method (makeResetString)
+   * -----------------------------------------------------
+   * @desc Makes an array of strings representing each value to be reset.
+   * @param {Array<string>=} settings - The settings to reset.
+   * @return {Array<string>} The array of strings to reset.
+   */
+  var makeResetString = (function setup_makeResetString() {
+
+    /** @type {!Object<string, string>} */
+    var props;
+    /** @type {string} */
+    var propName;
+
+    // A hash map of propName => propName
+    props = {
+      errorBreakpoints   : 'errorBreakpoints',
+      errorDebuggers     : 'errorBreakpoints',
+      className          : 'classTitle',
+      turnOffTypes       : 'turnOffMethods',
+      turnOnDebuggers    : 'addBreakpoints',
+      formatElementsAsObj: 'formatElementsAsObj'
+    };
+
+    for (propName in Debug_DEFAULTS) {
+      if ( hasOwnProp(Debug_DEFAULTS, propName) ) {
+        props[ propName ] = propName;
+      }
+    }
+
+    return function makeResetString(settings) {
+
+      /** @type {!Array<string>} */
+      var result;
+      /** @type {string} */
+      var prop;
+      /** @type {number} */
+      var len;
+      /** @type {number} */
+      var i;
+
+      if ( checkType(settings, '?null=') ) {
+        result = [];
+        // Add all props to the result
+        for (prop in props) {
+          if ( hasOwnProp(props, prop) ) {
+            result.push(props[ prop ]);
+          }
+        }
+      }
+      else {
+        if ( checkType(settings, '!strings') ) {
+          result = [];
+          // Add the correct props to the result
+          i = settings.length;
+          while (i--) {
+            prop = args[i];
+            if ( hasOwnProp(props, prop) ) {
+              result.push(props[ prop ]);
+            }
+          }
+        }
+        else {
+          result = null;
+        }
+      }
+
+      return result;
+    };
+  })();
+
+/* -----------------------------------------------------------------------------
  * The Debug Module API (module-api.js)
  * -------------------------------------------------------------------------- */
 
@@ -513,282 +789,6 @@ new TypeError(a);return!0}}();e.reset=function(){var b,a,c;b=(b=arguments.length
       }
 
       return !!args;
-    };
-  })();
-
-/* -----------------------------------------------------------------------------
- * The Public Module Variables (module-vars.js)
- * -------------------------------------------------------------------------- */
-
-  /**
-   * -----------------------------------------------------
-   * Public Variable (debugInstances)
-   * -----------------------------------------------------
-   * @desc Saves a reference to all of the created Debug instances.
-   * @type {!Object<string, !Debug>}
-   */
-  var debugInstances = {};
-
-  /**
-   * ----------------------------------------------- 
-   * Public Variable (errorBreakpoints)
-   * -----------------------------------------------
-   * @desc Controls whether debugger breakpoints are included with error logs.
-   * @type {boolean}
-   */
-  var errorBreakpoints = true;
-
-  /**
-   * -----------------------------------------------
-   * Public Variable (Debug_DEFAULTS)
-   * -----------------------------------------------
-   * @desc The original default settings for a new Debug instance.
-   * @type {!{
-   *   classTitle    : string,
-   *   turnOffMethods: string,
-   *   addBreakpoints: string,
-   *   turnOnGroups  : boolean,
-   *   openGroups    : boolean,
-   *   turnOnProfiles: boolean,
-   *   turnOnTimers  : boolean
-   * }}
-   */
-  var Debug_DEFAULTS = {
-    classTitle    : 'unknown',
-    turnOffMethods: 'none',
-    addBreakpoints: 'args fail',
-    turnOnGroups  : true,
-    openGroups    : false,
-    turnOnProfiles: false,
-    turnOnTimers  : false
-  };
-
-  /**
-   * -----------------------------------------------
-   * Public Variable (Debug_DEFAULT_TYPES)
-   * -----------------------------------------------
-   * @desc The data types for each settings property.
-   * @type {!Object<string, string>}
-   */
-  var Debug_DEFAULT_TYPES = {
-    classTitle    : 'string',
-    turnOffMethods: 'string|!strings',
-    addBreakpoints: 'string|!strings',
-    turnOnGroups  : 'boolean',
-    openGroups    : 'boolean',
-    turnOnProfiles: 'boolean',
-    turnOnTimers  : 'boolean'
-  };
-
-/* -----------------------------------------------------------------------------
- * The Public Module Methods (module-methods.js)
- * -------------------------------------------------------------------------- */
-
-  /**
-   * -----------------------------------------------------
-   * Public Method (makeNewDebugInst)
-   * -----------------------------------------------------
-   * @desc Creates a new Debug object instance. For more details about the
-   *   parameters [see debugModuleAPI.init]{@link debugModuleAPI#init}.
-   * @param {string} classTitle
-   * @param {Object} settings
-   * @param {(string|!strings)=} settings.turnOffMethods
-   * @param {(string|!strings)=} settings.addBreakpoints
-   * @param {boolean=} settings.turnOnGroups
-   * @param {boolean=} settings.openGroups
-   * @param {boolean=} settings.turnOnProfiles
-   * @param {boolean=} settings.turnOnTimers
-   */
-  function makeNewDebugInst(classTitle, settings) {
-
-    /** @type {!Object} */
-    var defaultTypes;
-    /** @type {!Object} */
-    var newSettings;
-    /** @type {!Object} */
-    var defaults;
-    /** @type {string} */
-    var propName;
-    /** @type {*} */
-    var propVal;
-
-    newSettings = {};
-    defaults = Debug.defaults;
-
-    // Set the new instance's settings to the defaults
-    for (propName in defaults) {
-      if ( hasOwnProp(defaults, propName) ) {
-        newSettings[ propName ] = defaults[ propName ];
-      }
-    }
-
-    // Update the new instance's settings with any local settings
-    if (settings) {
-      defaultTypes = Debug_DEFAULT_TYPES;
-      for (propName in settings) {
-        if (hasOwnProp(settings, propName) && hasOwnProp(defaults, propName)) {
-          propVal = settings[ propName ];
-          if ( checkType(propVal, defaultTypes[ propName ]) ) {
-            if ( checkType(propVal, '!strings') ) {
-              propVal = propVal.join(' ');
-            }
-            newSettings[ propName ] = propVal;
-          }
-        }
-      }
-    }
-
-    // Update the new instance's class title
-    newSettings.classTitle = classTitle;
-
-    // Setup and save the new Debug instance
-    debugInstances[ classTitle ] = new Debug(newSettings);
-  };
-
-  /**
-   * -----------------------------------------------------
-   * Public Method (insertErrorBreakpoint)
-   * -----------------------------------------------------
-   * @desc Handles whether a debugger breakpoint is inserted for an error.
-   * @return {boolean} Whether a breakpoint was inserted.
-   */
-  function insertErrorBreakpoint() {
-
-    if (errorBreakpoints) {
-      debugger;
-    }
-
-    return errorBreakpoints;
-  };
-
-  /**
-   * ---------------------------------------------------
-   * Public Method (freezeObj)
-   * ---------------------------------------------------
-   * @desc A shortcut for the Object.freeze method with a deep freeze option.
-   * @param {!(Object|function)} obj - The object to freeze.
-   * @param {boolean=} deep - Deep freeze the object. The default is false.
-   * @return {!(Object|function)} The frozen object.
-   */
-  var freezeObj = aIV.utils.freezeObj;
-
-  /**
-   * ---------------------------------------------------
-   * Public Method (hasOwnProp)
-   * ---------------------------------------------------
-   * @desc A shortcut for the Object.prototype.hasOwnProperty method.
-   * @param {!(Object|function)} obj - The object to check.
-   * @param {string} prop - The property to check.
-   * @return {boolean} The result of the check.
-   */
-  var hasOwnProp = aIV.utils.hasOwnProp;
-
-  /**
-   * ---------------------------------------------------
-   * Public Method (checkType)
-   * ---------------------------------------------------
-   * @desc Checks a value's data type against the given optional types.
-   * @param {*} val - The value to be evaluated.
-   * @param {string} type - A string of the data types to evaluate the value
-   *   against. For a complete list of acceptable strings
-   *   [see aIV.utils.checkType]{@link https://github.com/imaginate/algorithmIV-javascript-shortcuts/blob/master/src/pre-compiled-parts/js-methods/checkType.js}.
-   * @param {boolean=} noTypeValCheck - If true skips the data type string checks.
-   *   The default is false. Use to avoid duplicating checks.
-   * @return {boolean} The evaluation result.
-   */
-  var checkType = aIV.utils.checkType;
-
-  /**
-   * ---------------------------------------------------
-   * Public Method (isValidTypeString)
-   * ---------------------------------------------------
-   * @desc Evaluates whether a string is a valid data type string.
-   * @param {string} type - The string to evaluate.
-   * @return {boolean} The evaluation result.
-   */
-  var isValidTypeString = aIV.utils.isValidTypeString;
-
-  /**
-   * ---------------------------------------------------
-   * Public Method (getTypeOf)
-   * ---------------------------------------------------
-   * @desc A shortcut for the native typeof operator that additionally
-   *   distinguishes null, array, document, and element types from an
-   *   object type.
-   * @param {*} val - The value to get the typeof.
-   * @return {string} The value's type.
-   */
-  var getTypeOf = aIV.utils.getTypeOf;
-
-  /**
-   * -----------------------------------------------------
-   * Public Method (makeResetString)
-   * -----------------------------------------------------
-   * @desc Makes an array of strings representing each value to be reset.
-   * @param {Array<string>=} settings - The settings to reset.
-   * @return {Array<string>} The array of strings to reset.
-   */
-  var makeResetString = (function setup_makeResetString() {
-
-    /** @type {!Object<string, string>} */
-    var props;
-    /** @type {string} */
-    var propName;
-
-    // A hash map of propName => propName
-    props = {
-      errorBreakpoints   : 'errorBreakpoints',
-      errorDebuggers     : 'errorBreakpoints',
-      className          : 'classTitle',
-      turnOffTypes       : 'turnOffMethods',
-      turnOnDebuggers    : 'addBreakpoints',
-      formatElementsAsObj: 'formatElementsAsObj'
-    };
-
-    for (propName in Debug_DEFAULTS) {
-      if ( hasOwnProp(Debug_DEFAULTS, propName) ) {
-        props[ propName ] = propName;
-      }
-    }
-
-    return function makeResetString(settings) {
-
-      /** @type {!Array<string>} */
-      var result;
-      /** @type {string} */
-      var prop;
-      /** @type {number} */
-      var len;
-      /** @type {number} */
-      var i;
-
-      if ( checkType(settings, '?null=') ) {
-        result = [];
-        // Add all props to the result
-        for (prop in props) {
-          if ( hasOwnProp(props, prop) ) {
-            result.push(props[ prop ]);
-          }
-        }
-      }
-      else {
-        if ( checkType(settings, '!strings') ) {
-          result = [];
-          // Add the correct props to the result
-          i = settings.length;
-          while (i--) {
-            prop = args[i];
-            if ( hasOwnProp(props, prop) ) {
-              result.push(props[ prop ]);
-            }
-          }
-        }
-        else {
-          result = null;
-        }
-      }
-
-      return result;
     };
   })();
 
